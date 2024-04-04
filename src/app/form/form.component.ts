329998d094e1e6e311d8427951967e4ffca09c9b
lyonelgj <lyonelgarcia3@gmail.com>
2024-04-04 21:00:11 +0100

    Basic Version

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, FormControl, AbstractControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  protected form: FormGroup;
+  protected submitted = false;
 
+  constructor(private formBuilder: FormBuilder){
+      this.form = this.formBuilder.group({
+        title: ['', Validators.required],
+      });
+  }
+  
+  get f(): { [key: string ]: AbstractControl} {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (!this.form.invalid) console.log(JSON.stringify(this.form.value, null, 2)) 
+  }
 }
