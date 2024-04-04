b5d0a52ca9cf952756ab6257a8dfa849c2c90642
angaro073 <andrewgarciarodriguez04@gmail.com>
2024-04-04 21:03:15 +0100

    route fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { title: 'Home Page', path: "/home", component: HomeComponent },
+  { title: 'Home Page', path: "home", component: HomeComponent },
   // { title: 'Show Post', path: "/post/:id", component: },
   // { title: 'Create Post', path: "/create", component: },
   // { title: 'Update Post', path: "/update", component: },
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
   { title: 'Error 404', path: '**', component: PageNotFoundComponent },
 ];
