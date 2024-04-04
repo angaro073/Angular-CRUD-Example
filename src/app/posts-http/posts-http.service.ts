a3ea779d2e6759a69743ce333c0ddf80136067f6
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-04-04 17:31:12 +0100

    Created post type and PostsHttpService

---
+++ src/app/posts-http/posts-http.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsHttpService {
+	private readonly apiURL = "http://localhost:3000/posts";
+
+  constructor(private httpClient: HttpClient) {
+
+	}
+
+	getAllPosts(): Observable<Post> {
+		return this.httpClient.get<Post>(this.apiURL);
+	}
+
+	getPost(id: string): Observable<Post> {
+		return this.httpClient.get<Post>(`${this.apiURL}/${id}`);
+	}
+
+	createPost(title: string): Observable<Post> {
+		return this.httpClient.post<Post>(`${this.apiURL}`, `{"title": ${title}, "views": 0}`);
+	}
+
+	updatePost(id: string, title: string): Observable<Post> {
+		return this.httpClient.put<Post>(`${this.apiURL}/${id}`, `{"title": ${title}, "views": 0}`);
+	}
+	
+	deletePost(id: string) {
+		return this.httpClient.delete<Post>(`${this.apiURL}/${id}`);
+	}
+}
